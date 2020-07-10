35e6f1f32ddb968b157e47b5cb27f4af3947b8a7
cobraz <cobraz@cobraz.no>
2020-07-10 23:29:07 +0000

    Deploying to gh-pages from  @ 2936b6cbc4d9ace6e5d0f6fa5ebeb4c0f7e9688c 🚀

---
+++ precache-manifest.789ecfdd4d96bd112de211c646d30b0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff48b11a0a2628bbdc010bbb7272d2a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.713c624c1f77ad8a4265.bundle.js"
+  },
+  {
+    "url": "runtime~main.713c624c1f77ad8a4265.bundle.js"
+  },
+  {
+    "url": "vendors~main.713c624c1f77ad8a4265.bundle.js"
+  },
+  {
+    "url": "vendors~main.713c624c1f77ad8a4265.bundle.js.LICENSE.txt"
+  }
+]);
