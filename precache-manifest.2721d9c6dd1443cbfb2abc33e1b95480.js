bc84def58795d9a42499cc51eea001df8e0a256e
cobraz <cobraz@cobraz.no>
2020-10-20 21:06:27 +0000

    Deploying to gh-pages from  @ c638c00fc1d03f07951c73c1cf7d9ed779d0880e 🚀

---
+++ precache-manifest.2721d9c6dd1443cbfb2abc33e1b95480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29363a28225e846875f42223a285bad",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d09369d7ab9fdebd9b97.bundle.js"
+  },
+  {
+    "url": "runtime~main.d09369d7ab9fdebd9b97.bundle.js"
+  },
+  {
+    "revision": "1f5d1e1328fb1f4c902a",
+    "url": "static/css/main.ce0fdc95.chunk.css"
+  },
+  {
+    "url": "vendors~main.d09369d7ab9fdebd9b97.bundle.js"
+  },
+  {
+    "url": "vendors~main.d09369d7ab9fdebd9b97.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.237a691c2fc6ba4191b9dd8bd82a60b4.js"
+  "precache-manifest.2721d9c6dd1443cbfb2abc33e1b95480.js"
 );
 
 self.addEventListener('message', (event) => {
