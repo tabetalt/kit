2419677be9eb51d18c02c611c7ffddc447916d4e
Dmitry Kvokov <kvokovdl@gmail.com>
2020-11-05 16:19:57 +0200

    refactor(Table): Refactored Table component, added ability to pass plugin hooks

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,102 +1,80 @@
 /** @jsx jsx */
 /* eslint react/jsx-key: 0 */
 import React from 'react';
-import { jsx } from 'theme-ui';
-import { UseTableOptions, useTable } from 'react-table';
+import { jsx, ThemeUIStyleObject } from 'theme-ui';
+import { PluginHook, useTable, UseTableOptions } from 'react-table';
+export * from 'react-table';
 
-export function Table<D extends object = {}>({
-  children: _,
-  ...props
-}: React.PropsWithChildren<UseTableOptions<D>>) {
+const styles = {
+  table: { borderCollapse: 'collapse', width: '100%' } as ThemeUIStyleObject,
+  thead: { bg: 'highlight' },
+  tr: {
+    bg: '#FAFAFA',
+    borderBottom: '1px solid',
+    borderBottomColor: 'highlight',
+    '&:last-child': {
+      border: '0',
+    },
+  },
+  th: { p: 3, textAlign: 'left', fontWeight: 'normal' },
+  td: { p: 3 },
+};
+
+export interface TableProps {
+  options: UseTableOptions<{}>;
+  plugins?: PluginHook<{}>[];
+}
+
+export const Table: React.FC<TableProps> = ({ options, plugins }) => {
+  const tableInstance = useTable(options, ...(plugins || []));
+
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
-  } = useTable(props);
+  } = tableInstance;
 
   return (
-    // apply the table props
-    <table
-      {...getTableProps()}
-      sx={{ borderCollapse: 'collapse', width: '100%' }}
-    >
-      <thead
-        sx={{
-          bg: 'highlight',
-        }}
-      >
-        {
-          // Loop over the header rows
-          headerGroups.map((headerGroup) => (
-            // Apply the header row props
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {
-                // Loop over the headers in each row
-                headerGroup.headers.map((column) => (
-                  // Apply the header cell props
-                  <th
-                    {...column.getHeaderProps()}
-                    sx={{
-                      p: 3,
-                      textAlign: 'left',
-                      fontWeight: 'normal',
-                      width: column.width,
-                      minWidth: column.minWidth,
-                      maxWidth: column.maxWidth,
-                    }}
-                  >
-                    {
-                      // Render the header
-                      column.render('Header')
-                    }
-                  </th>
-                ))
-              }
-            </tr>
-          ))
-        }
+    <table {...getTableProps()} sx={styles.table}>
+      <thead sx={styles.thead}>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => {
+              const thSx = {
+                ...styles.th,
+                width: column.width,
+                minWidth: column.minWidth,
+                maxWidth: column.maxWidth,
+              } as ThemeUIStyleObject;
+              return (
+                <th {...column.getHeaderProps()} sx={thSx}>
+                  {column.render('Header')}
+                </th>
+              );
+            })}
+          </tr>
+        ))}
       </thead>
-      {/* Apply the table body props */}
       <tbody {...getTableBodyProps()}>
-        {
-          // Loop over the table rows
-          rows.map((row) => {
-            // Prepare the row for display
-            prepareRow(row);
-            return (
-              // Apply the row props
-              <tr
-                {...row.getRowProps()}
-                sx={{
-                  bg: '#FAFAFA',
-                  borderBottom: '1px solid',
-                  borderBottomColor: 'highlight',
-                  '&:last-child': {
-                    border: '0',
-                  },
-                }}
-              >
-                {
-                  // Loop over the rows cells
-                  row.cells.map((cell) => {
-                    // Apply the cell props
-                    return (
-                      <td {...cell.getCellProps()} sx={{ p: 3 }}>
-                        {
-                          // Render the cell contents
-                          cell.render('Cell')
-                        }
-                      </td>
-                    );
-                  })
-                }
-              </tr>
-            );
-          })
-        }
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} sx={styles.tr}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()} sx={styles.td}>
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
-}
+};
+
+Table.defaultProps = {
+  plugins: [],
+};
