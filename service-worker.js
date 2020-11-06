5d4d39a3cd4953d21b6c4a009f2556c3daae8b08
cobraz <cobraz@cobraz.no>
2020-11-06 16:23:48 +0000

    Deploying to gh-pages from  @ b65aa0ed2977eb12c3194fa06a3440635dc297ca 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4b99ed1ad6f0166896c3d7103bdc23ae.js"
+  "precache-manifest.03c170451309505f299a8dc3b3f83516.js"
 );
 
 self.addEventListener('message', (event) => {
